4d83d40272840147907c93eafae08e500df534bf
mattew8 <mattew4483@likelion.org>
2024-09-07 22:54:02 +0900

    ✨ add loading

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,8 +1,22 @@
-import { Spinner } from '@radix-ui/themes';
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { Flex, Heading, Spinner } from '@radix-ui/themes';
 
 const loading = () => {
-  return <></>;
+  const [isLoadingRequired, setIsLoadingRequired] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsLoadingRequired(true), 500);
+  }, []);
+
+  return (
+    <Flex direction="column" align="center" p="6">
+      {isLoadingRequired && (
+        <Heading size="6">{`도서 검색 중 입니다...`}</Heading>
+      )}
+      <Spinner size="3" mt="3" />
+    </Flex>
+  );
 };
 
 export default loading;
